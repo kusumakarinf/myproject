159445a53711d15bd443fc5b465fe5075a4d01c8
kusumakar <kusumakarinf@gmail.com>
2019-01-12 16:58:57 +0530

    2nd release

--- src/main/java/automation/delta/App.java
+++ src/main/java/automation/delta/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    	System.out.println( "Hello World!" );
+        
+    	//hello test
+    	//hello
+    	
+    	int a,b,c;
+    	a =33;
+    	b =444;
+    	c =a+b;
+    	System.out.println(c);
+    		
+        
     }
 }
